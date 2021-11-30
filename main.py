0f4856d540c8121bc143386834e677691e9d8b1d
vikrantarora14 <69387216+vikrantarora14@users.noreply.github.com>
2021-11-30 00:58:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+print("hello world")
+
+print("vikrant arora")
+
+print(9)
+
+# print out the even numbers from 2 to 18
+for i in range(2,18):
+  if(i%2==0):
+    print(i)
+
+#variables printing
+age = 26
+print(age)
+age = 6
+#print(age)
+
+#multiline comments
+"""
+print(9)
+print("vikrant")
+"""
+
+#print variables
+age = 6
+name="kiaan"
+vegetarian = True
+print(age)
+print(name)
+print(vegetarian)
+
+#operands
+a = 12
+b=60
+print(b-a)
+print(b*a)
+print(b/a)
+
+c=a+b
+print(c)
+
+d = 2*a + b
+print(d/12)
+
+for i in range(1,9):
+  j = i + 3
+  print(j)
+
+language = "english"
+year = 2015
+print("the language was " + language +". " + "the year was " + str(year) + ". " + "there was no escape.")
+
+print("the language was " + language +". " + "the year was" ,year, ". " + "there was no escape.")
+
+#extension questions
+
+#modulo
+print(60%12)
+print(60%7)
+
+#floor division //
+print(7//3)
+
+a=10;b=20;c=30
+
+print(a + b + c)
+
+d=e=f = 10
+
+print( d+ e + f)
+
+x = str(9) + "hello"
+print(x)
+
+print( True*20)
+
+import math
+
+for i in range(1,81):
+    if(math.isqrt(i)**2 == i):
+      print(i)
+
+print("hello"*2)
+print(3**2)
+
+
+
